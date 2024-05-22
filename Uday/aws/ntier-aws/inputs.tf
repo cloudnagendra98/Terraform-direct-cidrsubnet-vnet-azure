f7fa864617506996c068cba1711dc6cc89554888
cloudnagendra99 <cloudnagendra99@gmail.com>
2024-05-22 22:57:49 +0530

    Added vpc and subnets with [count.index]

--- Uday/aws/ntier-aws/inputs.tf
+++ Uday/aws/ntier-aws/inputs.tf
@@ -4,3 +4,25 @@ variable "vpc_network_cidr" {
   description = "This is vpc network range"
 
 }
+variable "web_subnet" {
+  type    = list(string)
+  default = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24", "10.10.4.0/24", "10.10.5.0/24"]
+
+}
+
+variable "subnets_tags" {
+  type    = list(string)
+  default = ["web1", "web2", "web3", "web4", "web5", "web6"]
+
+}
+variable "subnet_count" {
+  type    = number
+  default = 6
+
+}
+
+#variable "app_subnet" {
+# type = string
+#default = "10.10.1.0/24"
+
+#}
--- Uday/aws/ntier-aws/main.tf
+++ Uday/aws/ntier-aws/main.tf
@@ -4,3 +4,50 @@ resource "aws_vpc" "ntier_vpc" {
     Name = local.name
   }
 }
+resource "aws_subnet" "web" {
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.ntier_vpc.id
+  cidr_block = var.web_subnet[count.index]
+  tags = {
+
+    Name = var.subnets_tags[count.index]
+  }
+  depends_on = [
+    aws_vpc.ntier_vpc
+  ]
+}
+
+#resource "aws_subnet" "web" {
+# vpc_id     = aws_vpc.ntier_vpc.id
+#cidr_block = var.web_subnet
+#tags = {
+
+# Name = "udayweb"
+#}
+#depends_on = [
+# aws_vpc.ntier_vpc
+#]
+#}
+
+#resource "aws_subnet" "app" {
+# vpc_id = aws_vpc.ntier_vpc.id
+#cidr_block = var.app_subnet
+#tags = {
+# Name = "udayapp"
+#}
+#depends_on = [ 
+# aws_vpc.ntier_vpc 
+#]
+#}
+
+#resource "aws_subnet" "web" {
+# vpc_id     = aws_vpc.ntier_vpc.id
+# cidr_block = "10.10.0.0/24"
+# tags = {
+
+#  Name = "udayweb"
+# }
+# depends_on = [
+#   aws_vpc.ntier_vpc
+# ]
+#}
