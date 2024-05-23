319c7f434239f8a7ae14e2f5dd8e01768ee772a5
cloudnagendra99 <cloudnagendra99@gmail.com>
2024-05-23 18:57:18 +0530

    Added count.index to subnet tags and for cidr block to create multiple subnets at one go

--- Uday/aws/ntier-aws1/inputs.tf
+++ Uday/aws/ntier-aws1/inputs.tf
@@ -5,9 +5,29 @@ variable "aws_vpc_cidr" {
 
 }
 
-variable "aws_subnet_ids" {
-    type = string
-    default = "10.10.0.0/24"
-  
+
+variable "aws_subnets" {
+  type        = list(string)
+  default     = ["10.10.0.0/24", "10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24", "10.10.4.0/24", "10.10.5.0/24"]
+  description = "These are subnet ids"
+
 }
+
+variable "subnet_tags" {
+  type        = list(string)
+  default     = ["web1", "web2", "web3", "web4", "web5", "web6"]
+  description = "These are subnet name tags"
+
+}
+
+variable "subnet_count" {
+  type    = number
+  default = 6
+
+}
+
+#variable "aws_subnet_ids" {
+#   type = string
+#    default = "10.10.0.0/24"
+#}
 
--- Uday/aws/ntier-aws1/main.tf
+++ Uday/aws/ntier-aws1/main.tf
@@ -8,9 +8,22 @@ resource "aws_vpc" "ntier_vpc" {
 }
 
 resource "aws_subnet" "subnets" {
-    vpc_id = aws_vpc.ntier_vpc.id
-    cidr_block = var.aws_subnet_ids
-  depends_on = [ 
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.ntier_vpc.id
+  cidr_block = var.aws_subnets[count.index]
+  tags = {
+    Name = var.subnet_tags[count.index]
+  }
+  depends_on = [
     aws_vpc.ntier_vpc
-     ]
+  ]
 }
+
+
+#resource "aws_subnet" "subnets" {
+#   vpc_id = aws_vpc.ntier_vpc.id
+#  cidr_block = var.aws_subnet_ids
+#  depends_on = [ 
+#  aws_vpc.ntier_vpc
+# ]
+# }
